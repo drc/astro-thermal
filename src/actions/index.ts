131a9830f397231aa4e5e6b92e9f6fcff9d93fbb
Dan Cigrang <danielcigrang@me.com>
2025-01-19 21:32:34 +0000

    working

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,13 +1,31 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import { createCanvas, loadImage } from "canvas";
+import { client } from "@/lib/printer";
+import ReceiptPrinterEncoder from "@point-of-sale/receipt-printer-encoder";
+
+// @ts-ignore
+const encoder = new ReceiptPrinterEncoder({ createCanvas, imageMode: "raster", feedBeforeCut: 7 });
+
 
 export const server = {
     printPhoto: defineAction({
         input: z.object({
-            imageData: z.string(),
+            imageDataUrl: z.string(),
         }),
         handler: async (input, _context) => {
-            return `Hello, ${input.imageData}!`;
+            console.log("input", input);
+            const image = await loadImage(input.imageDataUrl);
+            const imagemessage = encoder
+                .align("center")
+                .image(image, image.width, image.height, "floydsteinberg")
+                .rule({ width: 42 })
+                .cut()
+                .encode();
+            console.log("imagemessage", imagemessage);
+            client.write(imagemessage);
+            // client.write(encoder.align("center").qrcode(input.imageDataUrl, { model: 1 }).cut().encode());
+            return `Photo printed!`;
         },
     }),
 };
