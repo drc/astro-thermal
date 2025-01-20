98daa42e6e4420da03f6cb728763026fbe79f49b
Dan Cigrang <danielcigrang@me.com>
2025-01-20 04:19:44 +0000

    working photobooth

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,10 +3,10 @@ import { z } from "astro:schema";
 import { createCanvas, loadImage } from "canvas";
 import { client } from "@/lib/printer";
 import ReceiptPrinterEncoder from "@point-of-sale/receipt-printer-encoder";
+import sharp from "sharp";
 
 // @ts-ignore
-const encoder = new ReceiptPrinterEncoder({ createCanvas, imageMode: "raster", feedBeforeCut: 7 });
-
+const encoder = new ReceiptPrinterEncoder({ createCanvas, imageMode: "raster", feedBeforeCut: 8 });
 
 export const server = {
     printPhoto: defineAction({
@@ -14,13 +14,16 @@ export const server = {
             imageDataUrl: z.string(),
         }),
         handler: async (input, _context) => {
-            const image = await loadImage(input.imageDataUrl);
-            const imagemessage = encoder
-                .align("center")
-                .image(image, image.width, image.height, "floydsteinberg")
-                .rule({ width: 42 })
-                .cut()
-                .encode();
+            // use sharp to import base64 image
+            const imageBuffer = Buffer.from(input.imageDataUrl.split(",")[1], "base64");
+            const sharpInstance = sharp(imageBuffer).sharpen({ sigma: 2 });
+            sharpInstance.png().toFile("./photo.png");
+            const processedBuffer = await sharpInstance.toBuffer();
+            const processedBase64 = `data:image/png;base64,${processedBuffer.toString("base64")}`;
+            const image = await loadImage(processedBase64);
+            const width = image.width;
+            const height = image.height;
+            const imagemessage = encoder.align("center").image(image, width, height, "atkinson").cut().encode();
             client.write(imagemessage);
             return `Photo printed!`;
         },
