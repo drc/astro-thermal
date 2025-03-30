0a25fcb6e4cf5fcf01c621e0b2172c2fdda36760
Dan Cigrang <danielcigrang@me.com>
2025-03-30 20:12:45 +0000

    allow accepting where the ip is coming from and the user agent, if they want to include a message as well

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -12,6 +12,9 @@ export const server = {
     printPhoto: defineAction({
         input: z.object({
             imageDataUrl: z.string().includes("data:image/png;base64,"),
+            message: z.string().optional(),
+            ip_address: z.string().optional(),
+            user_agent: z.string().optional(),
         }),
         handler: async (input, _context) => {
             try {
@@ -20,7 +23,6 @@ export const server = {
                     .metadata()
                     .then(({ width = 0, height = 0 }) =>
                         sharp(imageBuffer)
-                            .resize(Math.round(width * 0.9))
                             .sharpen({ sigma: 2 })
                     );
                 sharpInstance.png().toFile("./photo.png");
@@ -29,8 +31,15 @@ export const server = {
                 const image = await loadImage(processedBase64);
                 const width = image.width;
                 const height = image.height;
-                const imagemessage = encoder.align("center").image(image, width, height, "atkinson").cut().encode();
-                client.write(imagemessage);
+                const imagemessage = encoder.align("center").image(image, width, height, "atkinson").align("center");
+                if (input.message) {
+                    imagemessage.text(input.message);
+                    imagemessage.newline(2);
+                }
+                imagemessage.text(input.ip_address || "No IP Address");
+                imagemessage.newline(2);
+                imagemessage.text(input.user_agent || "No User Agent");
+                client.write(imagemessage.cut().encode());
                 return `Photo printed!`;
             } catch (e) {
                 console.error(e);
